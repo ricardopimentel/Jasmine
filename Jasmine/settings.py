720c78f176a78176a35a620e30e21e037aabcbeb
ricardopimentel <ricardoppimentel@gmail.com>
2017-07-03 10:52:10 -0300

    Banco de dados

--- Jasmine/settings.py
+++ Jasmine/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 
 import os
 from decouple import config
+from dj_database_url import parse as dburl
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -97,16 +98,10 @@ WSGI_APPLICATION = 'Jasmine.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
+default_dburl = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
 DATABASES = {
-     'default': {
-         'ENGINE': 'django.db.backends.mysql',
-         'NAME': 'jasmine',
-         'USER': 'root', #DIGITE USUARIO DO BANCO
-         'PASSWORD': 'root', #DIGITE SENHA DO BANCO
-         'HOST': 'localhost',
-         'PORT': '3306'
-         }
-     }
+    'default': config('DATABASE_URL', default=default_dburl, cast=dburl),
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
